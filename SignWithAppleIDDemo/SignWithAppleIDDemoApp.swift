356b3e0b00e2ddf6408e85025457f75320428bc8
chanoktrue <chanoktrue@me.com>
2023-12-02 16:01:24 +0700

    Initial Commit

---
+++ SignWithAppleIDDemo/SignWithAppleIDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignWithAppleIDDemoApp.swift
+//  SignWithAppleIDDemo
+//
+//  Created by Thongchai Subsaidee on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SignWithAppleIDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
